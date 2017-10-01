3bb1f9a6b5171f94ad6acd1bdc37e5f3a9300dd0
Rohan Suresh <rohansuresh27@gmail.com>
2017-10-01 22:35:18 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
 
-    window.location.replace("http://github.com"); 
+    //window.location.replace("http://github.com"); 
+    window.open("http://github.com");
     
   }, false);
 }, false);
+
 
 
